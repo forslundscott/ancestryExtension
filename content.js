120c9b9f1df661f6040b60b92b63e311a2618692
Scott Forslund <94809089+forslundscott@users.noreply.github.com>
2023-02-01 15:07:31 -0500

    added feature to navigate to potential ancestor

--- content.js
+++ content.js
@@ -28,11 +28,34 @@ function nGenerations(numberOfGenerations){
 
 }
 
+function findPotentialAncestor(){
+    if(document.getElementsByClassName("potential").length > 0){
+        var ancestorRectangle = document.getElementsByClassName("potential")[0].getBoundingClientRect();
+        var ancestorVerticalCenter = (ancestorRectangle.bottom - ancestorRectangle.top)/2
+        var ancestorHorizontalCenter = (ancestorRectangle.right - ancestorRectangle.left)/2
+        var treeWrapper = document.getElementsByClassName("treeWrapper")[0]
+        var containerWindow = document.getElementsByClassName("treeViewZoomPanContainer")[0].getBoundingClientRect()
+        var screenVerticalCenter = containerWindow.top + ((containerWindow.bottom - containerWindow.top)/2)
+        var screenHorizontalCenter = containerWindow.left + ((containerWindow.right - containerWindow.left)/2)
+        
+        var desiredTop = screenVerticalCenter - ancestorVerticalCenter
+        var desiredLeft = screenHorizontalCenter - ancestorHorizontalCenter
+
+        var verticalMovementRequired = desiredTop - ancestorRectangle.top
+        var horizontalMovementRequired = desiredLeft - ancestorRectangle.left
+        treeWrapper.style.top = +treeWrapper.style.top.match(/\d*\.*\d*/)[0] + verticalMovementRequired + 'px'
+        treeWrapper.style.left = +treeWrapper.style.left.match(/\d*\.*\d*/)[0] + horizontalMovementRequired + 'px'
+    }
+    
+    // console.log(rect.top, rect.right, rect.bottom, rect.left);
+}
+
 chrome.runtime.onMessage.addListener(gotMessage)
 
 function gotMessage(request, sender, sendResponse){
     if(document.location.href.match(/https:.*ancestry.com.family-tree.tree.*cfpid=\d*.*/)[0]==document.location.href){
         expandGeneration()
+        findPotentialAncestor()
     }
 }
 
